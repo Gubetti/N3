7b227ac1e231e41dfedb4bb52f1885828f8eb403
gubetti <gubetti@gmail.com>
2014-05-19 17:50:06 -0400

    Ajustes. Getters e Setters na classe ObjetoGrafico e Listener do Mouse na classe Frame.

--- src/Frame.java
+++ src/Frame.java
@@ -34,6 +34,8 @@ public class Frame extends JFrame{
 		add(canvas,BorderLayout.CENTER);
 		canvas.addGLEventListener(renderer);        
 		canvas.addKeyListener(renderer);
+		canvas.addMouseListener(renderer);
+		canvas.addMouseMotionListener(renderer);
 		canvas.requestFocus();			
 	}		
 	
--- src/model/ObjetoGrafico.java
+++ src/model/ObjetoGrafico.java
@@ -15,4 +15,52 @@ public class ObjetoGrafico {
 	
 	public void desenhar() {}
 
+	public Ponto[] getPontos() {
+		return pontos;
+	}
+
+	public void setPontos(Ponto[] pontos) {
+		this.pontos = pontos;
+	}
+
+	public Cor getCor() {
+		return cor;
+	}
+
+	public void setCor(Cor cor) {
+		this.cor = cor;
+	}
+
+	public int getPrimitiva() {
+		return primitiva;
+	}
+
+	public void setPrimitiva(int primitiva) {
+		this.primitiva = primitiva;
+	}
+
+	public BBox getBbox() {
+		return bbox;
+	}
+
+	public void setBbox(BBox bbox) {
+		this.bbox = bbox;
+	}
+
+	public ObjetoGrafico[] getFilhos() {
+		return filhos;
+	}
+
+	public void setFilhos(ObjetoGrafico[] filhos) {
+		this.filhos = filhos;
+	}
+
+	public Transformacao getTransformacao() {
+		return transformacao;
+	}
+
+	public void setTransformacao(Transformacao transformacao) {
+		this.transformacao = transformacao;
+	}
+	
 }
